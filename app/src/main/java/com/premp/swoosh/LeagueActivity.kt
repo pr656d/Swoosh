daf152052575d73206affa324d26c6b50bbf6b8a
Prem Patel <prempatel40p@gmail.com>
2018-02-03 17:27:32 +0530

    life cycle and log.d added.

--- app/src/main/java/com/premp/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/premp/swoosh/LeagueActivity.kt
@@ -3,10 +3,10 @@ package com.premp.swoosh
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class welcomeActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        setContentView(R.layout.activity_league)
     }
 }
